15234957a8895d9a64453b8593e8217483faef5c
CarlosGarciaPaez <cgarciapaez15@gmail.com>
2022-04-20 02:23:19 -0500

    First commit

---
+++ Reporte Anual/src/Class.java
@@ -0,0 +1,15 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("434160c5-4ba9-43a3-a505-24457d7e7fbf")
+public class Class {
+    @objid ("be9df411-392b-43aa-ba11-335632b75f09")
+    private String nombre;
+
+    @objid ("5fd9f6b5-3404-4f25-a359-d23a8d7d0ce9")
+    private int semestre;
+
+    @objid ("fa2913dc-79da-44e1-a991-e2524263268b")
+    public boolean registrar() {
+    }
+
+}
